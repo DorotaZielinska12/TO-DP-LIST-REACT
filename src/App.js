835487f5cb35feaa0a1c67b1deabbf258a96e08e
Dorota Zielinska <dodamad3@gmail.com>
2023-03-26 12:37:30 +0100

    toogleTaskDone interaction

--- src/App.js
+++ src/App.js
@@ -23,6 +23,16 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+
+      return task;
+    }));
+  }
+
   return (
     <Container>
       <Header title="Lista zadan" />
@@ -34,7 +44,12 @@ function App() {
       <Section
         title="Lista zadan"
         body={
-          <Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} />
+          <Tasks
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
         }
         extraHeaderContent={
           <Buttons
