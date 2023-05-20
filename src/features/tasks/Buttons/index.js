f48ecfefd5be28b37bd3ac4d4b7dba3ee9d9692e
Dorota Zielinska <dodamad3@gmail.com>
2023-05-20 14:55:13 +0100

    added redux to app

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,21 +1,32 @@
+import { useSelector, useDispatch } from "react-redux";
 import { Div, Button } from "./styled";
+import { toggleHideDone, setAllDone, selectAreTasksEmpty, selectHideDone, selectIsEveryTaskDone } from "../tasksSlice";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-    <Div>
-        {tasks.length > 0 && (
-            <>
-                <Button onClick={toggleHideDone}>
-                    {hideDone ? "Pokaz" : "Ukryj"} ukonczone
-                </Button>
-                <Button
-                    onClick={setAllDone}
-                    disabled={tasks.every(({ done }) => done)}
-                >
-                    Ukoncz wszystkie
-                </Button>
-            </>
-        )}
-    </Div>
-);
+const Buttons = () => {
+    const areTasksEmpty = useSelector(selectAreTasksEmpty);
+    const isEveryTaskDone = useSelector(selectIsEveryTaskDone);
+    const hideDone = useSelector(selectHideDone);
+
+    const dispatch = useDispatch();
+
+    return (
+        <Div>
+            {!areTasksEmpty && (
+                <>
+                    <Button onClick={() => dispatch(toggleHideDone())}>
+                        {hideDone ? "Pokaz" : "Ukryj"} ukonczone
+                    </Button>
+                    <Button
+                        onClick={() => dispatch(setAllDone())}
+                        disabled={selectIsEveryTaskDone}
+                    >
+                        Ukoncz wszystkie
+                    </Button>
+                </>
+            )}
+        </Div>
+    )
+};
+
 
 export default Buttons;
