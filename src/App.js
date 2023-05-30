095dcf58ead0ea928f56a3a8db94deb38d564c65
Dorota Zielinska <dodamad3@gmail.com>
2023-05-30 15:57:24 +0100

    added task list and author to app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import TaskComponent from "./features/tasks/TaskComponent";
+import Author from "./features/author/Author";
 
-export default () => <TaskComponent />
+export default () => (
+    <BrowserRouter basename="/TO-DO-LIST-REACT">
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/zadania">Zadania</Link>
+                </li>
+                <li>
+                    <Link to="/author">Autor</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route path="/zadania">
+                    <TaskComponent />
+                </Route>
+                <Route path="/author">
+                    <Author />
+                </Route>
+            </Switch>
+        </nav>
+    </BrowserRouter>
+);
---
+++ src/features/author/Author.js
@@ -0,0 +1,27 @@
+import Container from "../../common/Container"
+import { Header } from "../../common/Section/styled"
+import Section from "../../common/Section"
+
+export default () => (
+    <Container>
+        <Header title="O autorze" />
+        <Section
+            title="Dorota Zielinska"
+            body={
+                <>
+                    <p>
+                        Urodzilam sie w Polsce gdzie tez podjelam i ukonczylam edukacje.
+                        W wieku 21 postanowilam wyjechac do UK gdzie podjelam prace w swoim zawodzie jako krawcowa.
+                        Zaczelam tez nauke wieczorowa w pobliskim Colleague aby podciagnac sie z Angielskiego, tam tez poznalam Artura.
+                        Po jakims czasie wzielismy slub i urodzil nam sie syn Dominik.
+                        Niesty malzensto nie przetrwalo proby czasu wiec obecnie sama wychowuje syna.
+                        Nie koncze jednak drogi ku samodoskonaleniu dlatego tez zaczelam kurs programowania z YouCode.
+                        W wolnej chwili chodze na silownie i spa bo to moj czas dla siebie 😜
+                        Uwielbiam tez sluchac muzyke, spacerowac czy podrozowac po Swiecie a mam juz na koncie kilka ciekawych zakatkow 😉
+
+                    </p>
+                </>
+            }
+        />
+    </Container>
+);
