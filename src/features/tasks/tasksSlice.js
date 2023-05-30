59cffac4b2af250fe6a5f24322470c890b2f9b29
Dorota Zielinska <dodamad3@gmail.com>
2023-05-30 09:37:32 +0100

    persist tasks in localStorage

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -1,9 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { getTasksFromLocalSorage } from './tasksLocalStorage';
 
 const tasksSlice = createSlice({
     name: 'tasks',
     initialState: {
-        tasks: [],
+        tasks: getTasksFromLocalSorage(),
         hideDone: false,
     },
     reducers: {
